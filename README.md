b09a1409f516061d10c11f577102cb1a027b2bb1
Dominique Mattern <dom.mattern@gmail.com>
2020-01-04 22:26:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Node TTS Polly
+
+Convert text to speech using AWS Polly and Node.js.
+
+## Requirements
+
+- Node.js `>= 12.x`
+- FFmpeg
+- an Amazon Web Service (AWS) account
+
+## Installation
+
+### From GitHub Package Registry
+
+```
+$ npm install -g --registry https://npm.pkg.github.com/nullent1ty @nullent1ty/tts-polly
+```
+
+### From sources
+
+```
+$ git clone https://github.com/NullEnt1ty/node-tts-polly
+$ cd node-tts-polly
+$ npm install -g
+```
+
+## Usage
+
+Node TTS Polly can be called from the command line using `tts-polly`. Text is
+read from standard input and given out as wav sound using standard output.
+
+For example:
+
+```
+$ printf "This text will be synthesized" | tts-polly > synthesized-text.wav
+```
+
+Make sure to set the environment variable `AWS_SDK_LOAD_CONFIG` to a truthy
+value to load config and credentials from the `~/.aws` directory.
+
+```
+$ export AWS_SDK_LOAD_CONFIG=1
+```
+
+For more information see this article:
+https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
---
+++ src/app.ts
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+'use strict';
+
+import AWS from 'aws-sdk';
+import { SynthesizeSpeechInput } from 'aws-sdk/clients/polly';
+import { spawn } from 'child_process';
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import { Stream } from 'stream';
+
+const config = {
+  voice: 'Vicki',
+};
+
+function log(message: string): void {
+  process.stderr.write(`${message}\n`);
+}
+
+function getFilenameForText(text: string, voice: string, format: string): string {
+  const hash = crypto.createHash('sha1');
+  hash.update(text);
+
+  return `${voice}_${hash.digest('hex').toString()}.${format}`;
+}
+
+function getCacheDirPath(): string {
+  return 'cache';
+}
+
+function ensureCacheDirExists(): void {
+  const cacheDirPath = getCacheDirPath();
+
+  if (!fs.existsSync(cacheDirPath)) {
+    fs.mkdirSync(cacheDirPath);
+  }
+}
+
+function convertMp3ToWav(mp3Buffer: Buffer): Stream {
+  const args = ['-f', 'mp3', '-i', '-', '-f', 'wav', '-ac', '1', '-ar', '16k', '-c:a', 'pcm_s16le', '-'];
+  const ffmpegProcess = spawn('ffmpeg', args);
+
+  ffmpegProcess.stdin.write(mp3Buffer);
+  ffmpegProcess.stdin.end();
+
+  return ffmpegProcess.stdout;
+}
+
+async function streamToBeClosed(stream: Stream): Promise<void> {
+  return new Promise((resolve) => {
+    stream.on('close', () => {
+      resolve();
+    });
+  });
+}
+
+async function main() {
+  const stdinData = fs.readFileSync(0, 'utf8');
+
+  if (stdinData === undefined || stdinData === '') {
+    log('Expected text on stdin.');
+    process.exit(1);
+  }
+
+  const cacheDirPath = getCacheDirPath();
+  const filename = getFilenameForText(stdinData, config.voice, 'wav');
+  const cachedFilePath = path.join(cacheDirPath, filename);
+
+  if (fs.existsSync(cachedFilePath)) {
+    log('Found cached file!');
+
+    const cachedFileStream = fs.createReadStream(cachedFilePath);
+    cachedFileStream.pipe(process.stdout);
+
+    return;
+  }
+
+  log('No cached file found. Requesting speech data from AWS...');
+  ensureCacheDirExists();
+
+  const polly = new AWS.Polly({ apiVersion: '2016-06-10' });
+  const params: SynthesizeSpeechInput = {
+    OutputFormat: 'mp3',
+    Text: stdinData,
+    VoiceId: config.voice,
+  };
+
+  const result = await polly.synthesizeSpeech(params).promise();
+
+  if (result.AudioStream === undefined) {
+    log('Could not retrieve audio stream.');
+    process.exit(1);
+  }
+
+  const wavAudioStream = convertMp3ToWav(result.AudioStream as Buffer);
+  const wavCacheFileWriteStream = fs.createWriteStream(cachedFilePath);
+  wavAudioStream.pipe(wavCacheFileWriteStream);
+
+  await streamToBeClosed(wavCacheFileWriteStream);
+
+  const wavCacheFileReadStream = fs.createReadStream(cachedFilePath);
+  wavCacheFileReadStream.pipe(process.stdout);
+}
+
+main();
